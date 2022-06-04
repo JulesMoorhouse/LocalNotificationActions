ce12a47bccbb5352d1d2e39c90a8de8f9bb6c3ff
JulesMoorhouse <361024+JulesMoorhouse@users.noreply.github.com>
2022-06-04 09:55:24 +0100

    chore: basic notifications setup.

---
+++ LocalNotificationActions/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  LocalNotificationActions
+//
+//  Created by Jules Moorhouse on 04/06/2022.
+//
+
+import Foundation
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(
+        _: UNUserNotificationCenter,
+        willPresent _: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([[.banner, .badge, .sound]])
+    }
+}
+
--- LocalNotificationActions/ContentView.swift
+++ LocalNotificationActions/ContentView.swift
@@ -6,11 +6,49 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(action: {registerLocal()}, label: { Text("Register Local")})
+
+            Button(action: {scheduleLocal()}, label: { Text("Schedule Local")})
+        }
+    }
+    
+    func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Yay!")
+            } else {
+                print("Doh!")
+            }
+        }
+    }
+    
+    func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm"
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = .default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        
+        // 10:30 every morning aka repears
+        //let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
     }
 }
 
