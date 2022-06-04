289e65b2f7c3fb42f90fedb554a6f2cec0a8d15d
JulesMoorhouse <361024+JulesMoorhouse@users.noreply.github.com>
2022-06-04 09:17:46 +0100

    Initial Commit

---
+++ LocalNotificationActions/LocalNotificationActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationActionsApp.swift
+//  LocalNotificationActions
+//
+//  Created by Jules Moorhouse on 04/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
